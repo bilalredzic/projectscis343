6f0ba851130dfa3f105786b75249091b3f5b630f
Bilal Redzic <bilalredzic111@gmail.com>
2025-11-20 00:27:30 -0500

    project4 setup

---
+++ Project4/pokedex.js
@@ -0,0 +1,5 @@
+const readline = require("readline"); // Import the readline module to handle user input
+const rl = readline.createInterface({ // Create an interface for reading input and output
+    input: process.stdin, // Set the input to standard input (keyboard)
+    output: process.stdout // Set the output to standard output (console)
+});
